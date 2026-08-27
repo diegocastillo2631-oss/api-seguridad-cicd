7394e06241c7623d9d26f896de52e03af057b7f6
Diego Castillo <diegocastillo2631@gmail.com>
2026-08-27 10:53:32 -0500

    cambio prueva de q se ve

--- sample_app.py
+++ sample_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "API de Seguridad CI/CD funcionando correctamente",
+        "message": "API de Actualizada correctamente",
         "status": "ok"
     }), 200
 
